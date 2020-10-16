d6278558e2ddc4d5115f806fa1439d42ce3bea72
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-16 18:51:58 +0800

    Refactor look at cursor

---
+++ src/Modules/LookAtCursor/LookAtCursor.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace TopDownDemo.Modules.LookAtCursor
+{
+    public class LookAtCursor : Node2D
+    {
+        [Export] public NodePath HandlePath;
+
+        public Node2D Handle;
+
+        public override void _Ready()
+        {
+            Handle = GetNode<Node2D>(HandlePath);
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            HandleRotation();
+        }
+
+        private void HandleRotation()
+        {
+            var mouse = GetGlobalMousePosition();
+            Handle.LookAt(mouse);
+
+            // TODO: Should be handled by two side flipper
+            var angle = Mathf.Rad2Deg((mouse - Handle.GlobalPosition).Angle());
+            var shouldFlip = angle <= -90 || angle >= 90;
+
+            var scale = Handle.Scale;
+            scale.y = Mathf.Abs(scale.y) * (shouldFlip ? -1 : 1);
+
+            Handle.Scale = scale;
+        }
+    }
+}
---
+++ src/Modules/Module.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+namespace TopDownDemo.Modules
+{
+    public abstract class Module : Node2D
+    {
+    }
+}
